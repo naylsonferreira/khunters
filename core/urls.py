854bba50818cb01318ea6683052b4aadb62079fe
NaylsonFerreira <naylsonfsa@gmail.com>
2020-08-12 19:49:25 -0300

    fix router reset password

--- core/urls.py
+++ core/urls.py
@@ -19,12 +19,35 @@ urlpatterns = [
     path('singup/',singup,name='singup'),
     path('profile/up/<int:pk>/', login_required(ProfileUpdateView.as_view()), name='Update_profile'),
     path('profile/',login_required(ProfileListView.as_view()),name='profile'),
-    path('password_change/',auth_views.PasswordChangeView.as_view(template_name="core/password_change_form.html",success_url = reverse_lazy('core:password_change_done')),name='password_change'),
-    path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(template_name="core/password_change_done.html"),name='password_change_done'),
-    path('password_reset/',auth_views.PasswordResetView.as_view(template_name="core/password_reset_form.html"), name='password_reset'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name="core/password_reset_done.html"), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="core/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="core/password_reset_complete.html"), name='password_reset_complete'),
+    path('password_change/',
+        auth_views.PasswordChangeView.as_view(
+            template_name="core/password_change_form.html",
+            success_url = reverse_lazy('core:password_change_done')
+        ),name='password_change'),
+    path('password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="core/password_change_done.html"
+        ),name='password_change_done'),
+    path('password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name="core/password_reset_form.html",
+            email_template_name = "core/password_reset_email.html",
+            success_url = reverse_lazy('core:password_reset_done')
+        ),name='password_reset'),
+    path('password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="core/password_reset_done.html"
+        ),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="core/password_reset_confirm.html",
+            success_url = reverse_lazy('core:password_reset_complete')
+        ),name='password_reset_confirm'),
+    path('reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="core/password_reset_complete.html"),
+            name='password_reset_complete'
+        ),
     path('api/', include(router.urls)),
     path('',include(settings.DEFAULT_APP+'.urls',namespace=settings.DEFAULT_APP)),
 ]
